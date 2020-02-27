b166dd9582d22042f84eda94b019f4dbd910a6aa
James Bracy <waratuman@protonmail.com>
2020-02-27 15:09:45 -0800

    Adding libvips

---
+++ bin/support/libvips.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo "-----> Downloading libvips"
+if [ ! -s vips-8.9.1.tar.gz ]; then
+  cd $cache
+  curl -so vips-8.9.1.tar.gz \
+    https://github.com/libvips/libvips/releases/download/v8.9.1/vips-8.9.1.tar.gz \
+    | indent
+fi
+
+echo "-----> Unarchiving libvips"
+if [ ! -s vips-8.9.1 ]; then
+  cd $cache
+  mkdir -p vips-8.9.1
+  tar -xf vips-8.9.1.tar.gz
+fi
+
+echo "-----> Building libvips"
+if [ ! -s "$build/vendor/libvips" ]; then
+  cd $cache/vips-8.9.1
+  ./configure --prefix=$build/vendor/libvips | indent
+  make | indent
+  cd ..
+fi
+
+echo "-----> Installing SQLite"
+if [ ! -s "$build/vendor/libvips" ]; then
+  cd $cache/vips-8.9.1
+  make install | indent
+fi
---
+++ bin/support/sqlite3.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo "-----> Downloading SQLite"
+if [ ! -s sqlite-autoconf-3310100.tar.gz ]; then
+  cd $cache
+  curl -so sqlite-autoconf-3310100.tar.gz \
+    https://sqlite.org/2020/sqlite-autoconf-3310100.tar.gz \
+    | indent
+fi
+
+echo "-----> Unarchiving SQLite"
+if [ ! -s sqlite-autoconf-3310100 ]; then
+  cd $cache
+  mkdir -p sqlite-autoconf-3310100
+  tar -xf sqlite-autoconf-3310100.tar.gz
+fi
+
+echo "-----> Building SQLite"
+if [ ! -s "$build/vendor/sqlite3" ]; then
+  cd $cache/sqlite-autoconf-3310100
+  ./configure --prefix=$build/vendor/sqlite3 | indent
+  make | indent
+  cd ..
+fi
+
+echo "-----> Installing SQLite"
+if [ ! -s "$build/vendor/sqlite3" ]; then
+  cd $cache/sqlite-autoconf-3310100
+  make install | indent
+  echo "--with-sqlite3-dir=$build/vendor/sqlite3" >> $env_dir/BUNDLE_BUILD__SQLITE3
+  echo "$LD_LIBRARY_PATH:$build/vendor/sqlite3" >> $env_dir/LD_LIBRARY_PATH
+fi
